5fa1df3ee7d44a473aebb8dd97c22f0f576cb208
marceloanje <marcelo.anje@outlook.com>
2024-04-18 19:44:32 -0300

    feat: Implementation of the get tasks method

--- backend/src/models/taksModel.js
+++ backend/src/models/taksModel.js
@@ -1,4 +1,9 @@
-const getAll = () => {};
+const connection = require("./connection");
+
+const getAll = async () => {
+    const [tasks] = await connection.execute("SELECT * FROM tasks");
+    return tasks;
+};
 
 module.exports = {
     getAll,
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const tasksController = require("./controllers/taksController");
 
 const router = express.Router();
 
-// router.get("/tasks", (req, res) =>
-//     res.status(200).send("O router está funcionando!")
-// );
+router.get("/tasks", tasksController.getAll);
 
 module.exports = router;
